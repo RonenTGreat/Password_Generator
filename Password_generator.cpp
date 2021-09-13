dd633742ece987d3cf885c6c423263b94a1b5b6a
ronentgreat <ronenh53@gmail.com>
2021-09-13 00:21:11 +0000

    Good night!!!

---
+++ Password_generator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib> // Will use it to create random function
+
+using namespace std;
+
+int main(){
+    char lowerCases[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                           's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    char uppCase[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I','J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    char numbers[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+
+    // Create array of symbols
+
+
+    return 0;
+}
