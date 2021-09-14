efbd9aabc65c83c7a10da851067fef1683a7bb94
ronentgreat <ronenh53@gmail.com>
2021-09-14 11:08:53 +0000

    generates the password but the randomness does extend to all the arrays

--- Password_generator.cpp
+++ Password_generator.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
 #include <cstdlib> // Will use it to create random function
-#include <ctime>
+#include <ctime> // Use it for the time function
+
 
 using namespace std;
 
 int main(){
-    char lowerCases[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                           's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    const char characters[] = {'@', '#', '$', '1', '2', '3', 'a', 'b', 'c', 'd', 'e', 'f', 'g', '4', 's', 't', 'u', 'v',
+                               'w', '5', '6', '7', '8', '9', '0', '!','%', '^', '&', '*', '(', ')', '=', '?', 'h', 'i',
+                               'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
+                               'G', 'H', 'I','J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R','S', 'T', 'U', 'V', 'W', 'X',
+                               'Y', 'Z',};
 
-    char uppCase[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I','J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    int lengthOfCharacters = sizeof(characters) - 1;
 
-    char numbers[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+    cout << "Enter the length you want the password to be: " << endl;
 
-    // Create array of symbols
+    int lengthOfPassword;
+
+    cin >> lengthOfCharacters;
+    cout << "The length of password you want is " << lengthOfPassword << endl;
+    cout << endl;
+
+    srand(time(0)); // Uses the current time of the computer as the seed
+
+    cout << "Generated Password: ";
+
+    for(int i = 0; i < lengthOfCharacters; i++){
+        cout << characters[rand() % lengthOfCharacters];
+
+    }
+
+
+
 
 
     return 0;
