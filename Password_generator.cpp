a9b4014b338b17e3d39fa2062d0ce88d905e1f50
ronentgreat <ronenh53@gmail.com>
2021-09-14 11:43:58 +0000

    upgrade

--- Password_generator.cpp
+++ Password_generator.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(){
     const char characters[] = {'@', '#', '$', '1', '2', '3', 'a', 'b', 'c', 'd', 'e', 'f', 'g', '4', 's', 't', 'u', 'v',
-                               'w', '5', '6', '7', '8', '9', '0', '!','%', '^', '&', '*', '(', ')', '=', '?', 'h', 'i',
+                               'w', '5', '6', '7', '8', '9', '0', '!','%', '^', '&', '*', '=', '?', 'h', 'i',
                                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
                                'G', 'H', 'I','J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R','S', 'T', 'U', 'V', 'W', 'X',
                                'Y', 'Z',};
@@ -22,7 +22,7 @@ int main(){
     cout << "The length of password you want is " << lengthOfPassword<< endl;
     cout << endl;
 
-    srand(time(0)); // Uses the current time of the computer as the seed
+    srand(time(nullptr)); // Uses the current time of the computer as the seed
 
     cout << "Generated Password: ";
 
